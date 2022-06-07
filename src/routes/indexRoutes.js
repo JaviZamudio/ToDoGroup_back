bec836805c92ec5753cd9d01862324279cdc528e
JaviZamudio <javier.elihu100@gmail.com>
2022-06-07 13:44:00 -0500

    setting up controllers and routes

--- src/routes/indexRoutes.js
+++ src/routes/indexRoutes.js
@@ -1,5 +1,7 @@
 const router = require('express')();
 
 router.use("/users", require("./usersRoutes"));
+router.use("/tasks", require("./tasksRoutes"));
+router.use("/groups", require("./groupsRoutes"));
 
 module.exports = router;
